21a9795ac0c0e4de2e074d5d8e952e5b44f3c638
kimkyubae <kkyub5118@gmail.com>
2022-03-29 17:17:46 +0900

    interface

--- chapter14_object/src/Animal.kt
+++ chapter14_object/src/Animal.kt
@@ -1,2 +1,29 @@
 interface Animal {
+    fun walk()
+    fun eat()
+
+
+
+}
+
+class Dog: Animal{  // Animal뒤에 생성자가 없기 때문에 interface이다.
+    override fun walk() {
+        TODO("Not yet implemented")
+    }
+
+    override fun eat() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class Cat: Animal{
+    override fun walk() {
+        TODO("Not yet implemented")
+    }
+
+    override fun eat() {
+        TODO("Not yet implemented")
+    }
+
 }
