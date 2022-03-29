2b665900678368cab1fc5c539af3976ef8ae92b6
kimkyubae <kkyub5118@gmail.com>
2022-03-29 17:11:23 +0900

    direction

--- chapter14_object/src/Direction.kt
+++ chapter14_object/src/Direction.kt
@@ -1,2 +1,27 @@
-class Direction {
+import java.awt.GridBagConstraints.NORTHWEST
+
+enum class Direction(private  val str: String) {
+    NORTH("북"),
+    EAST("동"),
+    SOUTH("남"),
+    WEST("서");
+
+    val value: String = str
+}
+
+
+
+// 회원 레벨: Admin: 0 , gold: 1 , silver: 2 , normal: 3 ...  --> enum class USER_LEVEL { ADMIN, GOLD, SILVER, NORMAL}
+
+fun main(args: Array<String>) {
+    var direction = Direction.NORTH
+
+    println(direction.value)
+
+    direction = Direction.SOUTH
+    println(direction.value)
+
+    // 잘못된 값은 컴파일 에러
+    //    direction = 100 // 숫자는 넣지 못한다.
+    //    direction = Direction.NORTHWEST
 }
--- chapter14_object/src/User.kt
+++ chapter14_object/src/User.kt
@@ -1,6 +1,16 @@
 data class User(var name:String, var email:String, var phone:String)
 
-class User2(var name:String, var email:String, var phone:String)
+class User2(var name:String, var email:String, var phone:String) {
+    override fun toString(): String = "User2[name=$name, email=$email, phone,$phone]"
+    override fun equals(other: Any?): Boolean {
+        // this --> user3 , other --> user4
+
+        val o = other as User2 // 타입을 바꾸는 표현(타입 캐스팅) , o의 타입: User2
+
+        return name == other.name && email == other.email && phone == other.phone
+        return super.equals(other)
+    }
+}
 
 fun main(args: Array<String>) {
     val user1 = User("Kim", "Kim@email.com", "010-1234-1234")
@@ -11,6 +21,7 @@ fun main(args: Array<String>) {
 
     val user3 = User2("Kim", "Kim@email.com", "010-1234-1234")
     val user4 = User2("Kim", "Kim@email.com", "010-1234-1234")
+    println(user3.toString())
     println(user3)  // 내부적으로 toString 호출됨
     println(user3 == user4) // 내부적으로 equals 호출됨
 }
