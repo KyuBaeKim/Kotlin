f0feac5fc8545c91b35c4312e8683f7e177c92d8
kimkyubae <kkyub5118@gmail.com>
2022-03-29 14:15:26 +0900

    class

---
+++ chapter13_room/src/demo/room/Room.kt
@@ -0,0 +1,12 @@
+open class Room(val name: String){
+    fun description() = "Room: $name"
+
+    fun load() = "아무도 여기에 오지 않았습니다."
+}
+
+fun main(args: Array<String>) {
+    var currentRoom = Room("Foyer")
+
+    println(currentRoom.description())
+    println(currentRoom.load())
+}
